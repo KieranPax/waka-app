a48e1a8bf5f45e033be4cae5b1da3d7a3af06b07
KieranPax <kieranpax@gmail.com>
2021-07-23 16:55:41 +0100

    Fix router error message

--- src/App.vue
+++ src/App.vue
@@ -1,1 +1,10 @@
-<template><router-view /></template>
+<template>
+  <router-view>
+    <span style="position:absolute;left:20px;top:20px">
+      Page unavailable
+      <a href="/" style="color:lime;font-size:1.2em;">
+        Return to Home
+      </a>
+    </span>
+  </router-view>
+</template>
